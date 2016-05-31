393350d08da57a92ca61c61029d02411cd65d7f7
Gabriel Iuhasz <juhasz_gabriel@yahoo.com>
2016-05-31 17:19:17 +0300

    fixed range in bash script

--- dmon_ls_count.sh
+++ dmon_ls_count.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 thread=$1
 rt=$((thread-1))
-for i in $(seq 1 $rt);
+for i in $(seq 0 $rt);
 do
    echo "Number of messages received from Thread $i"
    cat results | grep -i "Thread id $i" | tee thread-$i.out | wc -l
