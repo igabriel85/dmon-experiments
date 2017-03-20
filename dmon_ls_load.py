c5adc135798556879364d42c7fc267c7bc1315e7
Gabriel Iuhasz <juhasz_gabriel@yahoo.com>
2017-03-20 22:34:29 +0200

    moved socket.close

--- dmon_ls_load.py
+++ dmon_ls_load.py
@@ -97,10 +97,10 @@ def tcp_worker2(ip, port, mSize, thID, wait=0):
             # print "Failed msg sending for thread %s message %s" %(str(thID), str(i))
             failedMsg.append(i)
             continue
-        sock.close()
         thread_times.append(time.time() - start)
         if wait:
             time.sleep(float(wait))
+    sock.close()
     # print thread_times
     # print sum(thread_times)
     totalTime = sum(thread_times)
