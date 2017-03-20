a489a5549d3a4dce799eb9cb7786dba9eaec4b62
Gabriel Iuhasz <juhasz_gabriel@yahoo.com>
2017-03-20 14:07:09 +0200

    added yarn and collectd message generator

---
+++ dmon_load_msg.py
@@ -0,0 +1,71 @@
+import os
+import json
+import random
+
+
+class DMonMSgGenerator:
+
+    collectd_msg = []
+    yarn_msg = []
+
+    def __init__(self, collectd_in, yarn_in):
+        self.collectd_in = collectd_in
+        self.yarn_in = yarn_in
+
+    def readCollectdMsg(self):
+        with open(self.collectd_in) as f:
+            content = f.readlines()
+        for event in content:
+            DMonMSgGenerator.collectd_msg.append(json.loads(event))
+        return DMonMSgGenerator.collectd_msg
+
+    def readyarnMsg(self):
+        with open(self.yarn_in) as f:
+            content = f.readlines()
+        for event in content:
+            jevent = json.loads(event)
+            msg = jevent['message']
+            DMonMSgGenerator.yarn_msg.append(msg)
+        return DMonMSgGenerator.yarn_msg
+
+    def genCollectdMsg(self, thID, msgNR, mrange):
+        '''
+        :param thID: ID of active thread
+        :param msgNR: number of message
+        :param mrange: range
+        :return: message
+        '''
+        msg = random.choice(DMonMSgGenerator.collectd_msg)
+        msg['Thread_ID'] = thID
+        msg['Msg_nr'] = msgNR
+        msg['Range'] = mrange
+        return str(msg)
+
+    def genYarnMsg(self, thID, msgNr, mrange):
+        '''
+        :param thID: ID of active thread
+        :param msgNR: number of message
+        :param mrange: range
+        :return:
+        '''
+        msg = random.choice(DMonMSgGenerator.yarn_msg)
+        threadString = 'Thread_ID=%s' %str(thID)
+        msgNrString = 'Msg_nr=%s' %str(msgNr)
+        msgRangeString = 'Range=%s' %str(mrange)
+        genMessage = ', '.join((msg, threadString, msgNrString, msgRangeString))
+        print genMessage
+
+    def dummyMsg(self, thID, msgNR, mrange):
+        return "Thread ID %s Message number %s range %s \n" % (str(thID), str(msgNR), str(mrange))
+
+test = DMonMSgGenerator('collectd_out.txt', 'yarn.out')
+#
+readTest = test.readCollectdMsg()
+# test.readyarnMsg()
+#
+print test.genCollectdMsg(1, 99, 100)
+# print test.dummyMsg(1, 99, 100)
+#
+# print test.genYarnMsg(1, 99, 100)
+
+
--- dmon_ls_load.py
+++ dmon_ls_load.py
@@ -4,7 +4,30 @@ import threading
 from random import *
 from collections import *
 import sys, getopt
+from dmon_load_msg import *
+import numpy
 
+MsgGenerator = DMonMSgGenerator('collectd_out.txt', 'yarn.out')
+
+MsgGenerator.readCollectdMsg()
+MsgGenerator.readyarnMsg()
+
+
+def median(lst):
+    '''
+    :param lst: list of values
+    :return: median
+    '''
+    return numpy.median(numpy.array(lst))
+
+
+def percentage(part, whole):
+    '''
+    :param part: part to calculate percentage
+    :param whole: all messages
+    :return: percentage
+    '''
+    return 100 * float(part)/float(whole)
 
 def udp_worker(ip, port, mSize, thID):
     '''
@@ -40,17 +63,64 @@ def tcp_worker(ip, port, mSize, thID):
     MESSAGE_BASE = "Thread ID %s Message number %s range %s \n"
     for i in range(0, mSize):
         # print "Sending message %s" %int(i)
-        sock.sendall(MESSAGE_BASE % (str(thID), i, mSize))
+        # sock.sendall(MESSAGE_BASE % (str(thID), i, mSize))
+        sock.sendall(MsgGenerator.genCollectdMsg(str(thID), i, mSize))
         #data = sock.recv(1024)
     sock.close()
     print 'Total in time for thread %s is -> %s' %(str(thID), str(time.time() - start))
 
 
+def tcp_worker2(ip, port, mSize, thID, wait=0):
+    '''
+    :param ip: ip of logstash
+    :param port: port of logstash tcp worker
+    :param mSize: size of the message (cardinality)
+    :param thID: thread id
+    :return:
+    '''
+    thread_times = []
+    print'Begin sending data from thread %s to port %d' % (str(thID), port)
+    for i in range(0, mSize):
+        start = time.time()
+        failedMsg = []
+        sock = socket.socket(socket.AF_INET,
+                             socket.SOCK_STREAM)
+        try:
+            sock.connect((ip, port))
+        except:
+            # print "Failed connection for thread %s message %s" %(str(thID), str(i))
+            failedMsg.append(i)
+            continue
+        try:
+            sock.sendall(MsgGenerator.genCollectdMsg(str(thID), i, mSize))
+        except:
+            # print "Failed msg sending for thread %s message %s" %(str(thID), str(i))
+            failedMsg.append(i)
+            continue
+        sock.close()
+        thread_times.append(time.time() - start)
+        if wait:
+            time.sleep(wait)
+    print thread_times
+    print sum(thread_times)
+    totalTime = sum(thread_times)
+    mediumTime = sum(thread_times)/len(thread_times)
+    mediantime = median(thread_times)
+    nrOfFailed = len(failedMsg)
+    failedPercent = percentage(nrOfFailed, mSize)
+    print 'Total  time for thread %s is -> %s' %(str(thID), str(totalTime))
+    print 'Average event time for thread %s is -> %s' %(str(thID), str(mediumTime))
+    print 'Median event time for thread %s is -> %s' %(str(thID), str(mediantime))
+    print 'Failed thread %s messages: %s' %(str(thID), str(nrOfFailed))
+    print 'Percentage of Failed Messages thread %s is : %s' %(str(thID), str(failedPercent))
+
+
+
 def main(argv):
     ip = '127.0.0.1'
     port = '5999'
-    PROCESS = 20
-    mSize = 100000000
+    PROCESS = 5
+    mSize = 100
     try:
         opts, args = getopt.getopt(argv, "he:p:t:m:", ["endpoint=", "port=", "threads=", "mCount="])
     except getopt.GetoptError:
@@ -87,13 +157,13 @@ if __name__ == '__main__':
     if len(sys.argv) == 1:
         workers = deque()
         PROCESS = 20
-        mSize = 100
+        mSize = 100000
 
         for i in range(0, PROCESS):
-            port = '5999'
+            port = '5000'
             ip = '85.120.206.27'
             #t = threading.Thread(target = udp_worker, args = [ip, port])   # comments this for testing tcp only
-            t = threading.Thread(target=tcp_worker, args=[ip, int(port), mSize, i])
+            t = threading.Thread(target=tcp_worker2, args=[ip, int(port), mSize, i])
             t.start()
             print("%s start" % t)
             workers.append(t)
